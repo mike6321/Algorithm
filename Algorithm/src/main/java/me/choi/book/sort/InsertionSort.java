9c8b03cc17a0b8ae47c9e0f9b10a01831f90a12c
junwoo <33277588+mike6321@users.noreply.github.com>
2020-08-20 02:01:08 +0900

    fix (삽입정렬)

---
+++ Algorithm/src/main/java/me/choi/book/sort/InsertionSort.java
@@ -0,0 +1,37 @@
+package me.choi.book.sort;
+
+/**
+ * Project : Algorithm
+ *
+ * @author : jwdeveloper
+ * @comment : 삽입정렬
+ * Time : 1:54 오전
+ */
+public class InsertionSort {
+    private static final int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+    public static void main(String[] args) {
+        int length = arr.length;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(j, j - 1);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static void swap(int min_index, int j) {
+        int temp = arr[min_index];
+        arr[min_index] = arr[j];
+        arr[j] = temp;
+    }
+}
