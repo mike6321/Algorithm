7f74d049d8ed28f875c0f7c612abec0b0fb4e54e
junwoo <33277588+mike6321@users.noreply.github.com>
2020-08-20 23:23:14 +0900

    fix (위에서 아래로 (삽입정렬))

---
+++ Algorithm/src/main/java/me/choi/book/sort/Problem_6_1.java
@@ -0,0 +1,41 @@
+package me.choi.book.sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Project : Algorithm
+ *
+ * @author : jwdeveloper
+ * @comment : 위에서 아래로
+ * Time : 10:28 오후
+ */
+public class Problem_6_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                if (arr[i] <= arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
