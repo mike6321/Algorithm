ae57a401a6df4e112de16fcbb8835a606fc01d62
junwoo <33277588+mike6321@users.noreply.github.com>
2020-08-25 00:46:10 +0900

    feat (모험가길드)

--- Algorithm/src/main/java/me/choi/book/e_problem/Problem_311.java
+++ Algorithm/src/main/java/me/choi/book/e_problem/Problem_311.java
@@ -1,5 +1,7 @@
 package me.choi.book.e_problem;
 
+import java.util.*;
+
 /**
  * Project : Algorithm
  *
@@ -7,8 +9,35 @@ package me.choi.book.e_problem;
  * @comment : 모험가 길드
  * Time : 11:29 오후
  */
-public class Problem_311 {
-    public static void main(String[] args) {
 
+/**
+5
+2 3 1 2 2
+ */
+public class Problem_311 {
+    private static int n;
+    private static List<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+
+        Collections.sort(list);
+
+        int result = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            count += 1;
+            if (count >= list.get(i)) {
+                result += 1;
+                count = 0;
+            }
+        }
+
+        System.out.println(result);
     }
 }
