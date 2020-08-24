e37212a4c315ee5dd3f6cb49ce021e8ad3bdaae2
junwoo <33277588+mike6321@users.noreply.github.com>
2020-08-25 00:50:33 +0900

    fix (모험가길드 코드 수정)

--- Algorithm/src/main/java/me/choi/book/e_problem/Problem_311.java
+++ Algorithm/src/main/java/me/choi/book/e_problem/Problem_311.java
@@ -1,8 +1,6 @@
 package me.choi.book.e_problem;
 
-import java.util.*;
-
-/**
+import java.util.Scanner; /**
  * Project : Algorithm
  *
  * @author : jwdeveloper
@@ -15,29 +13,26 @@ import java.util.*;
 2 3 1 2 2
  */
 public class Problem_311 {
-    private static int n;
-    private static List<Integer> list = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
-            list.add(scanner.nextInt());
+            arr[i] = scanner.nextInt();
         }
 
-        Collections.sort(list);
-
-        int result = 0;
         int count = 0;
+        int result = 0;
 
         for (int i = 0; i < n; i++) {
             count += 1;
-            if (count >= list.get(i)) {
-                result += 1;
+            if (arr[i] <= count) {
                 count = 0;
+                result += 1;
             }
         }
-
         System.out.println(result);
     }
+
 }
