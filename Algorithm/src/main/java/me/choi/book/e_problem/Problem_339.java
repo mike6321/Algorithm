401e171b187003aa82b9a34b3b8a086c2c209a9e
junwoo <33277588+mike6321@users.noreply.github.com>
2020-08-28 23:57:43 +0900

    chore

--- Algorithm/src/main/java/me/choi/book/e_problem/Problem_339.java
+++ Algorithm/src/main/java/me/choi/book/e_problem/Problem_339.java
@@ -4,54 +4,54 @@ import java.util.*;
 
 public class Problem_339 {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt(); // 도시의 개수
-        int M = scanner.nextInt(); // 도로의 개수
-        int K = scanner.nextInt(); // 거리정보
-        int X = scanner.nextInt(); // 출발도시의 번호
-        int[] arr = new int[300001];
+        int n = scanner.nextInt(); // 도시의 개수
+        int m = scanner.nextInt(); // 도로의 개수
+        int k = scanner.nextInt(); // 거리 정보
+        int x = scanner.nextInt(); // 출발 도시의 정보
+        int[] visited = new int[300001];
+        List<ArrayList<Integer>> graph = new ArrayList<>();
 
-        List<ArrayList<Integer>> lists = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            lists.add(new ArrayList<Integer>());
-            arr[i] = -1;
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Integer>());
+            visited[i] = -1;
         }
+        for (int i = 0; i < m; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
 
-        for (int i = 0; i < M; i++) {
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-
-            lists.get(x).add(y);
+            graph.get(a).add(b);
         }
-        arr[X] = 0;
-
+        visited[x] = 0;
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(X);
+        queue.offer(x);
 
         while (!queue.isEmpty()) {
-            Integer now = queue.poll();
+            int start = queue.poll();
+            int size = graph.get(start).size();
+            for (int i = 0; i < size; i++) {
+                int nextNode = graph.get(start).get(i);
 
-            for (int i = 0; i < lists.get(now).size(); i++) {
-                Integer nextNode = lists.get(now).get(i);
-
-                if (arr[nextNode] == -1) {
-                    arr[nextNode] = arr[now] + 1;
-                    queue.offer(nextNode);
+                if (visited[nextNode] == -1) {
+                    visited[nextNode] = visited[start] + visited[nextNode];
+                    queue.offer(visited[nextNode]);
                 }
             }
         }
 
-        boolean tag = true;
-        for (int i = 1; i <= N; i++) {
-            if (arr[i] == K) {
-                System.out.println(i);
-                tag = false;
+        boolean tag = false;
+
+        for (int i = 0; i < m; i++) {
+            if (visited[i] == k) {
+                tag = true;
+                System.out.println(visited[i]);
             }
         }
-        if (tag) {
+        if (!tag) {
             System.out.println(-1);
         }
-     }
+    }
 
 }
 
