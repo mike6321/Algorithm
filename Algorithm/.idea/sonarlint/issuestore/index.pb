113a7492e80746e7d66154809e4f7abffd85af7d
junwoo <33277588+mike6321@users.noreply.github.com>
2020-10-21 01:02:22 +0900

    N과 M (2) 재풀이

--- Algorithm/.idea/sonarlint/issuestore/index.pb
+++ Algorithm/.idea/sonarlint/issuestore/index.pb
@@ -139,3 +139,7 @@ f
 6src/main/java/me/choi/book/e_problem/impl/Chicken.java,5/d/5d6bea4d2485f49d60015e128daf61daf7bcb26d
 d
 4src/main/java/me/choi/book/e_problem/impl/Main2.java,6/1/614d523a7658ca841a659a9f8dfbe8efb6cb1a5e
+m
+=src/main/java/me/choi/book/e_problem/nandm/Problem_15650.java,0/1/01924a394f40d2f3d338e397548fd1f5f4d6e13c
+m
+=src/main/java/me/choi/book/e_problem/nandm/Problem_15649.java,8/3/83946f5af2f106868c92f5c88d3ab9842b718f5b
--- Algorithm/src/main/java/me/choi/book/e_problem/nandm/Problem_15650.java
+++ Algorithm/src/main/java/me/choi/book/e_problem/nandm/Problem_15650.java
@@ -3,20 +3,21 @@ package me.choi.book.e_problem.nandm;
 import java.util.Scanner;
 
 public class Problem_15650 {
+    private static int n;
+    private static int m;
     private static int[] arr;
     private static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        n = scanner.nextInt();
+        m = scanner.nextInt();
 
         arr = new int[m];
-        visited = new boolean[n];
-        combination(n, m, 1, 0);
+        dfs(1, 0);
     }
 
-    private static void combination(int n, int m, int at, int depth) {
+    private static void dfs(int index, int depth) {
         if (m == depth) {
             for (int value : arr) {
                 System.out.print(value + " ");
@@ -25,14 +26,9 @@ public class Problem_15650 {
             return;
         }
 
-        for (int i = at; i <= n; i++) {
+        for (int i = index; i <= n; i++) {
             arr[depth] = i;
-            //visited[i] = true;
-            combination(n, m,  i + 1, depth + 1);
-//            if (visited[i] == false) {
-//                //arr[depth] = i + 1;
-//                //visited[i] = false;
-//            }
+            dfs(i + 1, depth + 1);
         }
     }
 }
