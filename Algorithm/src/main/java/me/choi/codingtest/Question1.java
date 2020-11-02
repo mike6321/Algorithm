0a65792bcc4dc521b541e2bed324d1ea591e1d8e
junwoo <33277588+mike6321@users.noreply.github.com>
2020-11-02 22:53:00 +0900

    feat (11번가 코딩테스트)

--- Algorithm/src/main/java/me/choi/codingtest/Question1.java
+++ Algorithm/src/main/java/me/choi/codingtest/Question1.java
@@ -25,3 +25,17 @@ public class Question1 {
 //        ;
 //
 //        1  100
+
+//given the following data definition, write a query that selects all customer names together with the number of transactions
+//that they made. Customers with no transactions should be included as customers with 0 trnasactions.
+
+
+//implement a class Veterinarian that will be used as a part of a larger simulation of a veterinarian office.
+//
+//the class Veterinarian needs to bo efficient with respect to time used and contain the following methods.
+//
+//method1 : puts the specified pet at the end of the line.
+//method2 : removes the pet's name from the queue and returns it. If no pets are in the queue, null should be returned.
+//
+//For example, the following code snippet should write "Barkley", "Mittens":
+
