f265f63fbef4cbb7541dce170d898906e9278f9b
choijunwoo <mike6321@naver.com>
2020-12-17 10:42:15 +0900

    (Codility) MaxProductOfThree

--- Algorithm/src/main/java/me/choi/codility/MaxProductOfThree.java
+++ Algorithm/src/main/java/me/choi/codility/MaxProductOfThree.java
@@ -1,8 +1,6 @@
 package me.choi.codility;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class MaxProductOfThree {
     public static void main(String[] args) {
@@ -12,42 +10,24 @@ public class MaxProductOfThree {
     }
     public int solution(int[] A) {
         // write your code in Java SE 8
-        List<Point> list = new ArrayList<>();
-        for (int i = 0; i < A.length; i++) {
-            list.add(new Point(i, A[i]));
+        Arrays.sort(A);
+
+        int length = A.length - 1;
+
+        int result = 0;
+        int firstNum = A[0] * A[1];
+        int secondNum = A[length - 1] * A[length - 2];
+        int thirdNum = A[length];
+
+        if (thirdNum < 0) {
+            result = Math.min(firstNum, secondNum) * thirdNum;
+        }
+        else {
+            result = Math.max(firstNum, secondNum) * thirdNum;
         }
 
-        Collections.sort(list);
 
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).getValue());
-        }
-
-        int result = list.get(list.size() - 1).getValue() * list.get(list.size() - 2).getValue() * list.get(list.size() - 3).getValue();
-
-        System.out.println(result);
         return result;
     }
 }
-class Point implements Comparable<Point>{
-    private final int index;
-    private final int value;
 
-    public Point(int index, int value) {
-        this.index = index;
-        this.value = value;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    @Override
-    public int compareTo(Point o) {
-        return this.value - o.getValue();
-    }
-}
