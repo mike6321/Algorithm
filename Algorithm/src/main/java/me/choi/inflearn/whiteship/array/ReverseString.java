68bb839e50f3e18b5f307853a132fedf000e30e3
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-04 11:50:03 +0900

    배열4

---
+++ Algorithm/src/main/java/me/choi/inflearn/whiteship/array/ReverseString.java
@@ -0,0 +1,43 @@
+package me.choi.inflearn.whiteship.array;
+
+/**
+ * Project : Algorithm
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 11:35 오전
+ */
+public class ReverseString {
+    public static void main(String[] args) {
+        ReverseString reverseString = new ReverseString();
+        System.out.println(reverseString.solution1("Hello".toCharArray()));
+        // 시간복잡도 : O(N)
+        // 공간복잡도 : O(N)
+        System.out.println(reverseString.solution2("Hello".toCharArray()));
+        // 시간복잡도 : O(N)
+        // 공간복잡도 : O(1) -- 기존의 array를 그대로 사용한것이기 떄문에
+    }
+
+    private char[] solution1 (char[] message) {
+        char[] reversedMessage = new char[message.length];
+
+        for (int i = message.length - 1; i >= 0; i--) {
+             reversedMessage[message.length - 1 - i] = message[i];
+        }
+
+        return reversedMessage;
+    }
+
+    private char[] solution2 (char[] message) {
+        swap(message);
+        return message;
+    }
+
+    private void swap(char[] message) {
+        for (int i = 0; i < message.length / 2; i ++) {
+            char temp = message[i];
+            message[i] = message[message.length - 1 - i];
+            message[message.length - 1 - i] = temp;
+        }
+    }
+}
---
+++ Algorithm/src/main/java/me/choi/inflearn/whiteship/array/TwoSumSolution.java
@@ -0,0 +1,56 @@
+package me.choi.inflearn.whiteship.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Project : Algorithm
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 2:04 오후
+ */
+public class TwoSumSolution {
+    public static void main(String[] args) {
+        TwoSumSolution twoSumSolution = new TwoSumSolution();
+        int[] result = twoSumSolution.solution1(new int[]{2, 3, 5, 7}, 9);
+        System.out.println(Arrays.toString(result));
+
+        int[] result2 = twoSumSolution.solution2(new int[]{2, 3, 5, 7}, 9);
+        System.out.println(Arrays.toString(result2));
+        // 시간복잡도 : O(N)
+        // 공간복잡도 : O(N)
+    }
+
+    private int[] solution2(int[] numbers, int target) {
+        Map<Integer, Integer> numberMap = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numberMap.put(numbers[i], i);
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            int numberToFind = target - numbers[i];
+            if (numberMap.containsKey(numberToFind) && numberMap.get(numberToFind) != i) {
+                return new int[] {i, numberMap.get(numberToFind)};
+            }
+        }
+        return null;
+    }
+
+    private int[] solution1(int[] numbers, int target) {
+        //itar
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+
+
+}
