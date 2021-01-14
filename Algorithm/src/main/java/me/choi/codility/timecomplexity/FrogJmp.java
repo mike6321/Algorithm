198c89be30ad68bb3ea388d39291ec8828abf905
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-14 15:20:32 +0900

    FrogJmp

---
+++ Algorithm/src/main/java/me/choi/codility/timecomplexity/FrogJmp.java
@@ -0,0 +1,28 @@
+package me.choi.codility.timecomplexity;
+
+/**
+ * Project : Algorithm
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 3:13 오후
+ */
+public class FrogJmp {
+    public static void main(String[] args) {
+        FrogJmp frogJmp = new FrogJmp();
+        int X = 10;
+        int Y = 85;
+        int D = 30;
+        frogJmp.solution(X, Y, D);
+    }
+    public int solution(int X, int Y, int D) {
+        // write your code in Java SE 8
+        int result = (Y - X) / D;
+
+        if ((Y - X) % D != 0) {
+            result++;
+        }
+//        System.out.println(result);
+        return result;
+    }
+}
