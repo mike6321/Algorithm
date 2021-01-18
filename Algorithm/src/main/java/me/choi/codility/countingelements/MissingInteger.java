1500bd6fe89a72f4f11d27486a51c21aefed261a
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-18 19:53:52 +0900

    MissingInteger (100%)

--- Algorithm/src/main/java/me/choi/codility/countingelements/MissingInteger.java
+++ Algorithm/src/main/java/me/choi/codility/countingelements/MissingInteger.java
@@ -1,6 +1,5 @@
 package me.choi.codility.countingelements;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,36 +7,30 @@ import java.util.Set;
  * Project : Algorithm
  *
  * @author : jwdeveloper
- * @comment :
+ * @comment : HashSet은 중복을 허용하지 않고 자동정렬
  * Time : 7:10 오후
  */
 public class MissingInteger {
     public static void main(String[] args) {
         MissingInteger missingInteger = new MissingInteger();
 //        int[] A = {1, 3, 6, 4, 1, 2};
-//        int[] A = {1, 2, 3};
-        int[] A = {-1, -3};
+        int[] A = {1, 2, 3};
+//        int[] A = {-1, -3};
         int solution = missingInteger.solution(A);
         System.out.println(solution);
     }
     public int solution(int[] A) {
         // write your code in Java SE 8
-        Arrays.sort(A);
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < A.length; i++) {
             set.add(A[i]);
         }
-        int cnt = 1;
-        for (Integer i : set) {
-            if (i != cnt) {
-                return cnt;
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            if (!set.contains(i)) {
+                return i;
             }
-            if (i == set.size()) {
-                return cnt + 1;
-            }
-            cnt++;
         }
 
-        return 1;
+        return -1;
     }
 }
