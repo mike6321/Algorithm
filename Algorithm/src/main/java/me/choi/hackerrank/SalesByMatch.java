af3fcd348fef4af82406f995cb2820eefd29d435
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-19 10:53:53 +0900

    Hackerrank SalesByMatch

---
+++ Algorithm/src/main/java/me/choi/hackerrank/SalesByMatch.java
@@ -0,0 +1,52 @@
+package me.choi.hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ * Project : Algorithm
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 12:41 오전
+ */
+public class SalesByMatch {
+    static int sockMerchant(int n, int[] ar) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+
+        }
+        return 0;
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/Applications/WorkingDirectory/repository/input.txt"));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
