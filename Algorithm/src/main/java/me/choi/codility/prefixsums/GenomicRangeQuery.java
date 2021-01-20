9085d8c67c7f8380e245e4583cacea3dea054f28
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-20 21:14:52 +0900

    GenomicRangeQuery 재풀이

--- Algorithm/src/main/java/me/choi/codility/prefixsums/GenomicRangeQuery.java
+++ Algorithm/src/main/java/me/choi/codility/prefixsums/GenomicRangeQuery.java
@@ -1,13 +1,23 @@
 package me.choi.codility.prefixsums;
-
+import java.util.HashMap;
+import java.util.Map;
 /**
  * Project : Algorithm
  *
  * @author : jwdeveloper
  * @comment :
+ * 누적을 구해서
  * Time : 10:36 오후
  */
 public class GenomicRangeQuery {
+    private static Map<Character, Integer> impactFactor = new HashMap<Character, Integer>();
+
+    static {
+        impactFactor.put('A', 1);
+        impactFactor.put('C', 2);
+        impactFactor.put('G', 3);
+        impactFactor.put('T', 4);
+    }
 
     public static void main(String[] args) {
         String S = "CAGCCTA";
@@ -21,37 +31,55 @@ public class GenomicRangeQuery {
         }
     }
     public int[] solution(String S, int[] P, int[] Q) {
-        // write your code in Java SE 8
-        int length = S.length();
-        int impactLength = 4;
-        int[] impactCnt = new int[impactLength];
-        int[][] impactFactorArray = new int[impactLength][length + 1];
+        // write your code in Java SE 8a
+        int strLength = S.length();
+        int impactFactorLength = 4;
+        int[][] impactFactorArray = new int[impactFactorLength][strLength + 1];
 
-        char c;
-        int impactFactor = 0;
-        for (int i = 0; i < length; i++) {
-            c = S.charAt(i);
-            if ('A' == c) impactFactor = 1;
-            else if ('C' == c) impactFactor = 2;
-            else if ('G' == c) impactFactor = 3;
-            else if ('T' == c) impactFactor = 4;
 
-            impactCnt[impactFactor - 1] += 1;
+        // 문자열만큼 돌고
+        for (int i = 1; i <= strLength; i++) {
+            char targetStr = S.charAt(i-1);
+            int impactFactorIndex = impactFactor.get(targetStr);
 
-            for (int j = 0; j < impactLength; j++) {
-                impactFactorArray[j][i + 1] = impactCnt[j];
+            //네번
+            for (int j = 0; j < impactFactorLength; j++) {
+                if (impactFactorIndex == j + 1) {
+                    impactFactorArray[j][i] = impactFactorArray[j][i-1] + 1;
+                } else {
+                    impactFactorArray[j][i] = impactFactorArray[j][i-1];
+                }
             }
         }
+        for (int i = 0; i < impactFactorLength; i++) {
+            for (int j = 0; j < strLength + 1; j++) {
+                System.out.print(impactFactorArray[i][j] + " ");
+            }
+            System.out.println();
+        }
 
-        int[] result = new int[P.length];
-        for (int i = 0; i < P.length; i++) {
-            for (int j = 0; j < impactLength; j++) {
+
+
+        int length = P.length;
+        int[] result = new int[length];
+        System.out.println("##############################");
+        for (int i = 0; i < length; i++) {
+//    		int p = P[i];
+//    		int q = Q[i];
+            for (int j = 0; j < impactFactorLength; j++) {
+                System.out.println("[" + i + "," + j +"] "+ "[" + j + "," + (Q[i]+1) + "] " + "[" + j +"," + P[i] + "] => "+impactFactorArray[j][Q[i] + 1]  + ", " + impactFactorArray[j][P[i]]);
+                // 순서대로 올라가기 때문에 뒤에 증가되는것은 신경쓸필요 없고 그냥 break 하면 된다.
                 if (impactFactorArray[j][Q[i] + 1] - impactFactorArray[j][P[i]] > 0) {
                     result[i] = j + 1;
                     break;
                 }
             }
+            System.out.println("##############################");
         }
+
+
+
+
         return result;
     }
 }
