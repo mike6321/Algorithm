a68466dd6e3187c57ed8c5c6d81b95c34f22a34c
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-27 12:31:42 +0900

    스택 뒤집기 - 재귀

--- Algorithm/src/main/java/me/choi/whiteship/stack/Problem01.java
+++ Algorithm/src/main/java/me/choi/whiteship/stack/Problem01.java
@@ -18,13 +18,19 @@ public class Problem01 {
         stack.push(1);
         stack.push(2);
         stack.push(3);
-        stack.push(4);
-        stack.push(5);
 
-        Stack<Integer> reversedStack = solution1(stack);
-        System.out.println(reversedStack);
+//        Stack<Integer> reversedStack1 = solution1(stack);
+//        System.out.println(reversedStack1);
+
+        Problem01 problem01 = new Problem01();
+        problem01.solution2(stack);
+        System.out.println(stack);
     }
 
+    // TODO: [스택 비재귀] junwoochoi 2021/01/27 11:57 오전
+    /**
+     * 스택 비재귀
+    * */
     private static Stack<Integer> solution1(Stack<Integer> stack) {
         Stack<Integer> reversedStack = new Stack<>();
         while (!stack.isEmpty()) {
@@ -32,5 +38,41 @@ public class Problem01 {
         }
 
         return reversedStack;
+    }
+
+    // TODO: [스택 재귀] junwoochoi 2021/01/27 12:02 오후
+    /**
+     * 스택 재귀
+     * 재귀를 생각할때는 작은 단위로 생각하라
+     *
+     * 1 2 3
+     *
+     * solution2 - 1 2 3
+     * solution2 - 1 2   - pop(3)
+     * solution2 - 1     - pop(2)
+     * solution2 -       - pop(1)
+     *
+     * insertAtBottom - null      , 1  [1]
+     * insertAtBottom - [1]     , 2
+     * insertAtBottom -
+     * */
+    private void solution2(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+
+        int temp = stack.pop();
+        solution2(stack);
+        insertAtBottom(stack, temp);
+    }
+
+    private void insertAtBottom(Stack<Integer> stack, int number) {
+        if (stack.isEmpty()) {
+            stack.push(number);
+        } else {
+            int temp = stack.pop();
+            insertAtBottom(stack, number);
+            stack.push(temp);
+        }
     }
 }
